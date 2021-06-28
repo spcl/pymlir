6a4634b3f7dde9b003d2c1371fc7d1f3295681b6
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-06-28 09:21:00 -0500

    register linalg dialect

--- mlir/dialects/__init__.py
+++ mlir/dialects/__init__.py
@@ -1,6 +1,7 @@
 from .affine import affine as affine_dialect
 from .standard import standard as std_dialect
 from .scf import scf as scf_dialect
+from .linalg import linalg
 
 
-STANDARD_DIALECTS = [affine_dialect, std_dialect, scf_dialect]
+STANDARD_DIALECTS = [affine_dialect, std_dialect, scf_dialect, linalg]
