ce898530e905b32beda4656a321c7cca65ca9912
Amanda Tang <amandatang88888@gmail.com>
2023-11-13 13:21:49 -0500

    Update scf.for syntax

--- mlir/dialects/scf.py
+++ mlir/dialects/scf.py
@@ -5,7 +5,7 @@ import sys
 from mlir.dialect import Dialect, DialectOp, is_op, UnaryOperation
 import mlir.astnodes as mast
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, List, Tuple
 
 
 @dataclass
@@ -13,10 +13,15 @@ class SCFForOp(DialectOp):
     index: mast.SsaId
     begin: mast.SsaId
     end: mast.SsaId
+    step: mast.SsaId
     body: mast.Region
-    step: Optional[mast.SsaId] = None
-    _syntax_ = ['scf.for {index.ssa_id} = {begin.ssa_id} to {end.ssa_id} {body.region}',
-                'scf.for {index.ssa_id} = {begin.ssa_id} to {end.ssa_id} step {step.ssa_id} {body.region}']
+    iter_args: Optional[List[Tuple[mast.SsaId, mast.Type]]] = None
+    iter_args_types: Optional[List[mast.Type]] = None
+    out_type: Optional[mast.Type] = None
+    _syntax_ = ['scf.for {index.ssa_id} = {begin.ssa_id} to {end.ssa_id} step {step.ssa_id} {body.region}',
+                'scf.for {index.ssa_id} = {begin.ssa_id} to {end.ssa_id} step {step.ssa_id} : {out_type.type} {body.region}',
+                'scf.for {index.ssa_id} = {begin.ssa_id} to {end.ssa_id} step {step.ssa_id} iter_args {iter_args.argument_assignment_list_parens} -> {iter_args_types.type_list_parens} {body.region}',
+                'scf.for {index.ssa_id} = {begin.ssa_id} to {end.ssa_id} step {step.ssa_id} iter_args {iter_args.argument_assignment_list_parens} -> {iter_args_types.type_list_parens} : {out_type.type} {body.region}']
 
 
 @dataclass
